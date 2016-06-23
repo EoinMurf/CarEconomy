2f9cc02f89fda10dbd44a1c5f79e93c4bf8000e6
ebinpaiva <ebin.paiva@gmail.com>
2016-06-23 18:08:04 +0100

    up to OOPS

--- testdropdown.php
+++ testdropdown.php
@@ -1,14 +1,14 @@
+
 <?php
 	$dbname = "cars";
   	$hostname = "localhost";
   	$username = "root";
   	$password = "password";
 	$port = "3306";
-	
-	
-	$make = 'bmw'; //$_POST["make"]; 
-	$year = 2000;//$_POST["year"]; 
 
+	$make = $_POST["make"]; 
+	$year = $_POST["year"]; 
+	
 	@ $db = new mysqli("$hostname", "$username", "$password", "$dbname", "$port");
 	if (mysqli_connect_errno()) {
 		echo 'Error: Could not connect to database.  Please try again later.';
@@ -16,18 +16,16 @@
 	}
 	else{
 		$query1 = "select model.modelName from model inner join make on model.makeid = make.idmake inner join years on make.idYear = years.idyear where make.MakeName = '$make' && years.yearName ='$year'";
-		$query0 = "select model.modelName from cars.model makeid = 3'";
-							
-		echo "The query is : ".$query1;
+		//$query0 = 'SELECT modelName FROM cars.model  inner join make on model.makeid = make.idmake where make.makeName = "bmw"';
 		$result = $db->query($query1);
 		$count = mysqli_num_rows($result);
 	
-	for($i=0;$i < $count;$i++)
+		for($i=0;$i < $count;$i++)
 		{
 			$row = mysqli_fetch_row($result);
-			$modelss[] = $row[0];
+			$makes[] = $row[0];
 		}
-		echo JSON_encode($modelss);
+		echo JSON_encode($makes);
 		mysqli_close($db);	 
 	}
 ?>
